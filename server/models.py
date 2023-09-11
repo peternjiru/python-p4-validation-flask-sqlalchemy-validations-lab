2749b1bbf87b38731c20e120e287c0da4e5f1f37
Peter Njiru <peternjirum@gmail.com>
2023-09-10 20:53:50 -0400

    completes lab

--- server/models.py
+++ server/models.py
@@ -12,6 +12,18 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("failed empty name validation")
+        return name
+      
+    @validates('phone_number')
+    def validate_phone(self, key, phone):
+        if len(phone) < 10:
+            raise ValueError("failed phone number length validation")
+        return phone
+
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -26,6 +38,34 @@ class Post(db.Model):
     summary = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("failed empty title validation")
+
+        keywords = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(keyword in title for keyword in keywords):
+            raise ValueError("Failed clickbait validation")
+        return title
+
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError("failed content length validation")
+        return content
+       
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) >= 250:
+            raise ValueError("failed summary length validation")
+        return summary 
+     
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ["Fiction", "Non-Fiction"]:
+            raise ValueError("failed category validation")
+        return category 
 
 
     def __repr__(self):
